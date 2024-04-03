2962c584cebaa156924ab12f0a1681250be481ce
Chintu Rawat <chinturawat1112@gmail.com>
2024-04-04 02:23:58 +0530

    main with comments explanation

--- Main.java
+++ Main.java
@@ -4,3 +4,18 @@ public class Main {
         System.out.println("Addition "+Calculator.add(10, 15));
     }
 }
+/*
+master
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+ */
+
+/*
+calculator
+System.out.println("Addition "+Calculator.add(10, 15));
+ */
+
+/*
+only store the difference not store entire file
+ */
