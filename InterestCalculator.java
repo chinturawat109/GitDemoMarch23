e9fb85e0b3a42a7e990045ef4b1800c536415c40
Chintu Rawat <chinturawat1112@gmail.com>
2024-04-15 08:53:40 +0530

    Added interest calculator

---
+++ InterestCalculator.java
@@ -0,0 +1,5 @@
+public class InterestCalculator {
+    public static int calculate(int principle, int rate, int years){
+        return (principle*rate*years)/100;
+    }
+}
---
+++ calculate.java
@@ -0,0 +1,5 @@
+public class calculate {
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
