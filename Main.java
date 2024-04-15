b0572a9ae651e39485f282d05f0d5418116e0cd1
Chintu Rawat <chinturawat1112@gmail.com>
2024-04-15 16:39:33 +0530

    Added Interest Calculator

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Addition : "+ calculate.add(15,25));
-        System.out.println("Interest : "+ InterestCalculator.calculate(10000, 8, 6));
+        System.out.println("Addition -: "+ calculate.add(15,25));
+        System.out.println("Interest -: "+ InterestCalculator.calculate(10000, 8, 6));
 
     }
 }
